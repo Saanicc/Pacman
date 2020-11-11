18e8a001331ba99f1ace9a62bafc472f1324968b
ChabbeH <72022949+ChabbeH@users.noreply.github.com>
2020-11-11 21:30:52 +0100

    Pacman method created

--- app/src/main/java/com/example/pacman/DrawGame.java
+++ app/src/main/java/com/example/pacman/DrawGame.java
@@ -1,16 +1,15 @@
 package com.example.pacman;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Point;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Display;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.WindowManager;
 
 
 public class DrawGame extends SurfaceView implements Runnable, SurfaceHolder.Callback {
@@ -25,6 +24,16 @@ public class DrawGame extends SurfaceView implements Runnable, SurfaceHolder.Cal
     private int screenWidth;
     private long frameTicker;
     private int totalFrame = 4;
+    private int viewDirection = 2;
+    private Bitmap[] pacManUp, pacManRight, pacManDown, pacManLeft;
+    private Paint paint;
+    private int currentPacManFrame = 0;
+    private int xPosPacman;
+    private int yPosPacman;
+    private int xPosGhost;
+    private int yPosGhost;
+
+
 
 
     public DrawGame(Context context) {
@@ -33,16 +42,21 @@ public class DrawGame extends SurfaceView implements Runnable, SurfaceHolder.Cal
         thread = new Thread(this);
         thread.start();
         frameTicker = 1000 / totalFrame;
+
         createTileMap();
+
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
         ((GameActivity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
 
         screenWidth = displayMetrics.widthPixels;
         Log.d("TEST", String.valueOf(screenWidth));
+        loadBitmapImages();
 
         TILE_SIZE = screenWidth / 17;
         Log.d("TEST", String.valueOf(TILE_SIZE));
+        xPosPacman = 8 * TILE_SIZE;
+        yPosPacman = 13 * TILE_SIZE;
     }
 
     @Override
@@ -56,6 +70,7 @@ public class DrawGame extends SurfaceView implements Runnable, SurfaceHolder.Cal
             if (canvas != null) {
                 canvas.drawColor(Color.YELLOW);
                 drawMap(canvas);
+                drawPacMan(canvas);
                 updateFrame(System.currentTimeMillis());
                 holder.unlockCanvasAndPost(canvas);
             }
@@ -67,8 +82,13 @@ public class DrawGame extends SurfaceView implements Runnable, SurfaceHolder.Cal
         if (gameTime > frameTicker + (totalFrame * 30)) {
             frameTicker = gameTime;
             Log.d("TEST", "FPS: " + frameTicker);
-        }
 
+            currentPacManFrame++;
+            if (currentPacManFrame >= totalFrame) {
+                currentPacManFrame = 0;
+            }
+
+        }
     }
 
     public void drawMap(Canvas canvas) {
@@ -101,6 +121,24 @@ public class DrawGame extends SurfaceView implements Runnable, SurfaceHolder.Cal
         }
     }
 
+    public void drawPacMan(Canvas canvas){
+        switch (viewDirection){
+            case 0:
+                canvas.drawBitmap(pacManUp[currentPacManFrame],xPosPacman, yPosPacman, paint);
+                break;
+            case 1:
+                canvas.drawBitmap(pacManLeft[currentPacManFrame],xPosPacman,yPosPacman,paint);
+                break;
+            case 2:
+                canvas.drawBitmap(pacManRight[currentPacManFrame],xPosPacman,yPosPacman,paint);
+                break;
+            default:
+                canvas.drawBitmap(pacManDown[currentPacManFrame],xPosPacman,yPosPacman,paint);
+        }
+
+
+    }
+
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         Log.d("TEST", "Surface Created");
@@ -115,6 +153,55 @@ public class DrawGame extends SurfaceView implements Runnable, SurfaceHolder.Cal
     public void surfaceDestroyed(SurfaceHolder holder) {
         Log.d("TEST", "Surface Destroyed");
     }
+
+    private void loadBitmapImages(){
+        int spriteSize = screenWidth/17;
+        spriteSize = (spriteSize/5) * 5;
+
+        pacManRight = new Bitmap[totalFrame];
+        pacManRight[0] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(),R.drawable.pacman_right1), spriteSize, spriteSize, false);
+        pacManRight[1] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_right2), spriteSize, spriteSize, false);
+        pacManRight[2] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_right3), spriteSize, spriteSize, false);
+        pacManRight[3] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_right), spriteSize, spriteSize, false);
+        // Add bitmap images of pacman facing down
+        pacManDown = new Bitmap[totalFrame];
+        pacManDown[0] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_down1), spriteSize, spriteSize, false);
+        pacManDown[1] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_down2), spriteSize, spriteSize, false);
+        pacManDown[2] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_down3), spriteSize, spriteSize, false);
+        pacManDown[3] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_down), spriteSize, spriteSize, false);
+        // Add bitmap images of pacman facing left
+        pacManLeft = new Bitmap[totalFrame];
+        pacManLeft[0] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_left1), spriteSize, spriteSize, false);
+        pacManLeft[1] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_left2), spriteSize, spriteSize, false);
+        pacManLeft[2] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_left3), spriteSize, spriteSize, false);
+        pacManLeft[3] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_left), spriteSize, spriteSize, false);
+        // Add bitmap images of pacman facing up
+        pacManUp = new Bitmap[totalFrame];
+        pacManUp[0] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_up1), spriteSize, spriteSize, false);
+        pacManUp[1] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_up2), spriteSize, spriteSize, false);
+        pacManUp[2] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_up3), spriteSize, spriteSize, false);
+        pacManUp[3] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.pacman_up), spriteSize, spriteSize, false);
+
+//        ghostBitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+//                getResources(), R.drawable.ghost), spriteSize, spriteSize, false);
+    }
+
 
     public void createTileMap() {
         tileMap = new int[][] {
